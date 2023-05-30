7761288cb58cee3bf23b553b259f512b8fad182a
Sakura Kano <kanosakura@icloud.com>
2023-05-30 20:46:28 +0800

    bello yellow

--- 1. Build a Passenger Counter App/06. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/06. Reassigning and incrementing/index.js
@@ -7,4 +7,12 @@ console.log(count)
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
 
+let bonusPoints = 50
+bonusPoints = bonusPoints + 100
+bonusPoints = bonusPoints - 25
+bonusPoints = bonusPoints + 70
+
+console.log(bonusPoints)
+
+
 
